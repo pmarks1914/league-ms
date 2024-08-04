76a4d33e159f9077969dc7c65b210cab6689760b
Patrick Eli <pmarks1914@gmail.com>
2024-08-04 00:00:42 +0000

    transcript file handling

--- fileManager/fileManager.py
+++ fileManager/fileManager.py
@@ -15,6 +15,9 @@ s3 = boto3.client(
 # Initialize S3 client
 # s3 = session.client('s3')
 
+#    - Identification Document (Passport, ID Card)
+#    - Previous Credential Evaluation Reports (if any)
+#    - Letters of Recommendation (if applicable)
 
 def fileUploadManager(request, user_id, *args):
     if 'cert' not in request.files:
@@ -28,6 +31,7 @@ def fileUploadManager(request, user_id, *args):
             doc_type = "Certificate"
             if str(request.form.get('type')) == "2":
                 doc_type = "Transcript"
+                
             doc_format = file.content_type.split('/')[1]
             data_object = Fileupload.createFile(file.filename, file.filename, doc_type, doc_format, user_id)
             # save it to the folder
